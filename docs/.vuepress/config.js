a974038dcf98682043ffebc0bfc55f33147f1bf7
suxun <2635850288@qq.com>
2019-02-15 15:23:38 +0800

    :memo: homepage

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,8 @@
 module.exports = {
+  title: "NoteBook JS",
+  description: "🚀 JavaScript 学习笔记",
   base: "/notebook-js/",
+  head: [["link", { rel: "icon", href: "/favicon.ico" }]],
   themeConfig: {
     nav: [
       {
