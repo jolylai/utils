8c15319a9cce0da8d850e4443c3532df5969bd96
jolylai <jolylai163@163.com>
2021-09-23 17:19:39 +0800

    feat(entry): entry

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
-
-import after from './function/after.js'
-import before from './function/before.js'
-import bind from './function/bind.js'
-import bindKey from './function/bindKey.js'
-import curry from './function/curry.js'
-import defer from './function/defer.js'
-import delay from './function/delay.js'
-import flip from './function/flip.js'
-import memoize from './function/memoize.js'
-import once from './function/once.js'
-import overArgs from './function/overArgs.js'
-import rearg from './function/rearg.js'
-import chunk from './array/chunk.js'
-import difference from './array/difference.js'
-import differenceBy from './array/differenceBy.js'
-import flatten from './array/flatten.js'
+import after from "./function/after.js";
+import before from "./function/before.js";
+import bind from "./function/bind.js";
+import bindKey from "./function/bindKey.js";
+import curry from "./function/curry.js";
+import defer from "./function/defer.js";
+import delay from "./function/delay.js";
+import flip from "./function/flip.js";
+import memoize from "./function/memoize.js";
+import once from "./function/once.js";
+import overArgs from "./function/overArgs.js";
+import rearg from "./function/rearg.js";
+import chunk from "./array/chunk.js";
+import difference from "./array/difference.js";
+import differenceBy from "./array/differenceBy.js";
+import flatten from "./array/flatten.js";
 
 export {
   after,
@@ -32,5 +31,5 @@ export {
   chunk,
   difference,
   differenceBy,
-  flatten
-}
+  flatten,
+};
